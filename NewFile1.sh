03982d497fa5de39f22859086c6827133552d2b8
Talgat Tairov <tairovtalgat@gmail.com>
2022-12-24 21:42:22 +0600

    creation role notes

---
+++ NewFile1.sh
@@ -0,0 +1,17 @@
+dpkg --purge --force-all docker docker-engine docker.io containerd runc 
+apt update
+apt install -y \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+          
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null\
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
